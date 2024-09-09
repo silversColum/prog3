b4bfc3b3b5133d6e18450b39c3207ab926ab96a0
Logan Silvers <silvers@colum.edu>
2024-09-09 17:09:03 -0500

    Add files via upload

---
+++ Engine.cs
@@ -0,0 +1,23 @@
+﻿namespace LSiversSprint0
+{
+    public class Engine
+    {
+        public bool IsStarted = false;
+        public string About()
+        {
+            return "\n";
+        }
+        public Engine() { 
+            IsStarted = false;
+        }
+
+        public void Start()
+        {
+            IsStarted = true;
+        }
+        public void Stop()
+        {
+            IsStarted = false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+
+namespace LSiversSprint0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tester t = new Tester();
+            t.Test();
+        }
+
+        class Tester
+        {
+            public void Test()
+            {
+                Console.WriteLine("Flying Vehicle Tester......................................................");
+                Console.WriteLine("\nAirlplane.cs...............................................................");
+
+                /*
+                 * Airplane class tests
+                 * many of the airplane class methods return a string we will write these to the console for testing
+                 */
+                Airplane ap = new Airplane();
+                //Air plane should inherit from AerialVehicle
+                Console.WriteLine(ap.About());
+                /* Output AirplaneAbout:
+                 * This OOPFlyingVehicle.Airplane has a max altitude of 41000 ft.
+                 * It's current altitude is 0 ft.
+                 * OOPFlyingVehicleMidterm.Airplane engine is not started
+                 */
+
+                Console.WriteLine("\nAireplaneTakeOffTests...............................................................");
+                Console.WriteLine("\nCall ap.TakeOff():");
+                //Test take off should fail engine isn't started
+                Console.WriteLine(ap.TakeOff());  //Don't take off engine isn't started
+                /* Output:
+                 * OOPFlyingVehicleMidterm.Airplane can't fly it's engine is not started.
+                 */
+                Console.WriteLine("\nCall ap.StartEngine():");
+                ap.StartEngine();
+                Console.WriteLine(ap.TakeOff());  //take off engine is started
+                /* Output:
+                 * OOPFlyingVehicleMidterm.Airplane is flying
+                 */
+
+                //Fly up
+                Console.WriteLine("\nFly up Tests...................................................................");
+                Console.WriteLine("Call ap.FlyUp() fly to 1,000ft default");
+                ap.FlyUp();    //Fly up tp 1,000 ft
+                Console.WriteLine(ap.About());
+                Console.WriteLine("\nCall ap.FlyUp(44000) Fly up to 45,000ft:");
+                ap.FlyUp(44000);    //Fly up tp 45,000 ft shouldn't work
+                Console.WriteLine(ap.About());
+                Console.WriteLine("\nCall ap.FlyUp(44000) Fly up another 40,000ft shouldn't work");
+                ap.FlyUp(40000);    //Fly up tp 41,000 ft shouldn't work
+                Console.WriteLine(ap.About());
+                /*
+                 * Output:
+                 */
+
+                //Land
+                Console.WriteLine("\nFly Down.................................................................");
+                Console.WriteLine("Call ap.FlyDown(50000) Fly Down 50,000 ft");
+                ap.FlyDown(50000);   //Land by floying down 50,000 ft = Crash and shouldn't work
+                Console.WriteLine(ap.About());
+                Console.WriteLine("Call ap.FlyDown(ap.CurrentAltitude) this should land");
+                ap.FlyDown(ap.CurrentAltitude); //Land by flying down current altitiute
+                Console.WriteLine(ap.About());
+            }
+        }
+    }
+}
